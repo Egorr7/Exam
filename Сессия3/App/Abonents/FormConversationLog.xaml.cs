d3bbaf85a65f92d1af44ca6b6a9d1b989abf53f0
Бердовский Егор Геннадьевич <is119berdovskiy@oat.local>
2021-12-21 12:54:43 +0600

    добавил 3ю сессию

---
+++ Сессия3/App/Abonents/FormConversationLog.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Abonents
+{
+    /// <summary>
+    /// Логика взаимодействия для FormConversationLog.xaml
+    /// </summary>
+    public partial class FormConversationLog : Window
+    {
+        public FormConversationLog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBackToForm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddAbonent_Click(object sender, RoutedEventArgs e)
+        {
+            FormConversation formConversation = new FormConversation();
+            formConversation.Show();
+        }
+
+        private void btnChangeAbonent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDeleteAbonent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
